49ab50a9155cd3b5799745c332f278a09706c177
brandonroberts58 <51340741+brandonroberts58@users.noreply.github.com>
2019-06-03 13:13:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+//Need to make function that gets rid of preloader
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish') ;
+});
+function validateForm() {
+  var fName = document.forms["contactForm"]["fname"].value;
+  if (fName == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  var fEmail = document.forms["contactForm"]["femail"].value;
+  if (fEmail == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  var fPhone = document.forms["contactForm"]["fphone"].value;
+  if (fPhone == "") {
+    alert("Phone number must be filled out");
+    return false;
+  }
+  var fDetails = document.forms["contactForm"]["fdetails"].value;
+  if (fDetails == "") {
+    alert("Details must be filled out");
+    return false;
+  }
+};
