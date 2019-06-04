63a6d46fa447298a350df06d9531425957128807
brandonroberts58 <51340741+brandonroberts58@users.noreply.github.com>
2019-06-04 11:43:54 -0700

    Add files via upload

--- form-to-email.php
+++ form-to-email.php
@@ -1,17 +1,20 @@
 <?php
-		if (isset($_POST['submit'])) {
-			$name = $_POST['name'];
-			$subject = $_POST['subject'];
-			$mailFrom = $_POST['mail'];
-			$message = $_POST['message'];
+		$name = $_POST['name'];
+		$visitor_email = $_POST['email'];
+		$message = $_POST['message'];
 
-			$mailTo = "test@brontevillagevet.com";
-			$headers = "From: ".$mailFrom;
-			$txt = "You have received an email from ".$name.".\n\n".$message;
+			$email_from = 'test@brontevillagevet.com';
 
-			mail($mailTo, $subject, $txt, $headers);
+			$email_subject = "New Form Submission";
 
-			header("Location: index.php?mailsend");
-		}
+			$email_body = "User Name: $name.\n".
+							"User Email: $visitor_email.\n".
+									"User Message: $message.\n";
 
+			$to = "test@brontevillagevet.com";
+			$headers = "From: $email_from \r\n";
+			$headers .= "Reply-To: $visitor_email \r\n";
+
+			mail($to,$email_subject,$email_body,$headers);
+			header("Location: index.html");
 ?>
